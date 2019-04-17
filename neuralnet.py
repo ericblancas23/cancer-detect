f462f4e2f645faa81f5d6399152bad62ee1f8458
EricB <ericblancas23@gmail.com>
2019-04-17 10:35:30 -0700

    finished neural net

--- neuralnet.py
+++ neuralnet.py
@@ -8,8 +8,11 @@ attributes = data.data
 labels = data.target
 
 attributes_train, attributes_test, labels_train, labels_test = train_test_split(attributes, labels, test_size=0.33)
-
+#set up neural net
 neuralnet = MLPClassifier()
 neuralnet.fit(attributes_train, labels_train)
 accuracy = neuralnet.score(attributes_test, labels_test)
+print(str(accuracy(* 100) + "% accuracy")
+#replace the original instantiation
+neuralnet = MLPClassifier(solver='lbfgs', activation='logistic', alpha=10.0)
 
