2dd6c25155322182fe6d64a4063f574cf1d05103
EricB <ericblancas23@gmail.com>
2019-04-16 22:51:38 -0700

    conducted neural net

--- neuralnet.py
+++ neuralnet.py
@@ -2,6 +2,14 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 
+#split data into training and testing sets
 data = load_breast_cancer()
 attributes = data.data
 labels = data.target
+
+attributes_train, attributes_test, labels_train, labels_test = train_test_split(attributes, labels, test_size=0.33)
+
+neuralnet = MLPClassifier()
+neuralnet.fit(attributes_train, labels_train)
+accuracy = neuralnet.score(attributes_test, labels_test)
+
